0fccef6b9f0872ea10f4dff23d0fb923a3964e86
Nangeso-Roy <jmnangeso@gmail.com>
2021-08-07 09:34:22 +0300

    Installed bootstrap and activated

--- Portfolio-Website/resources/views/blog/create.blade.php
+++ Portfolio-Website/resources/views/blog/create.blade.php
@@ -7,6 +7,30 @@
                 <a href="/blog" class="btn btn-outline-primary btn-sm">Return</a>
                 <div class="border rounded mt-5 pl-4 pr-4 pt-4 pb-4">
                     <h1 class="display-4">Write something New</h1>
+
+                    <hr>
+
+                    <form action="" method="post">
+                        @csrf
+                        <div class="row">
+                            <div class="control-group col-12">
+                                <label for="title">Blog Title</label>
+                                <input type="text" id="title" class="form-control" name="title" 
+                                  placeholder="Write the title here"required>
+                            </div>
+                            <div class="control-group col-12 mt-2">
+                                <label for="body">Blog here</label>
+                                <textarea name="body" id="body" cols="30" rows="" class="form-control" placeholder="Write here.Blow them away"></textarea>
+                            </div>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="control-group col=12 text-center">
+                                <button type="submit" id="btn=submit" class="btn btn-primary">
+                                    Create Blog
+                                </button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
 
